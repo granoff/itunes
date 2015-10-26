e2485692f4a586b9d27b815f266aa858791c16bb
Dirk de Kok <dirk@mobtest.com>
2015-10-25 20:02:22 -0700

    cleaned up

--- lib/itunes/request.rb
+++ lib/itunes/request.rb
@@ -6,12 +6,10 @@ module ITunes
 
     # Perform an HTTP GET request
     def request(request_type, params)
-      puts "about to load"
       url = '/WebObjects/MZStoreServices.woa/wa/ws' + request_type
 
       response = connection.get do |req|
         req.url url, params
-        #req.options = request_options
         req.options.timeout = 5
         req.options.open_timeout = 2
       end
